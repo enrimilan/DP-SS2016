100e807c668e24dfd3dd289ee93a436ce5962a98
Enri Miho <enrimiho@gmail.com>
2016-06-15 17:55:27 +0200

    check if vulcanoes and earthquakes correlate

--- src/main/java/org/tuwien/experiment/App.java
+++ src/main/java/org/tuwien/experiment/App.java
@@ -1,6 +1,7 @@
 package org.tuwien.experiment;
 
 import org.tuwien.experiment.Utils.Utils;
+import org.tuwien.experiment.entity.Correlation;
 import org.tuwien.experiment.entity.Earthquake;
 import org.tuwien.experiment.entity.Volcano;
 
@@ -14,6 +15,28 @@ public class App {
         }
         ArrayList<Volcano> volcanoes = Utils.parseVolcanoes(args[0]);
         ArrayList<Earthquake> earthquakes = Utils.parseEarthquakes(args[1]);
+        checkForCorrelationBetween(volcanoes, earthquakes, 15000);
+    }
+
+    public static ArrayList<Correlation> checkForCorrelationBetween(ArrayList<Volcano> volcanoes, ArrayList<Earthquake> earthquakes, double maxDist) {
+        int index = 0;
+        ArrayList<Correlation> correlations = new ArrayList<>();
+        for(Volcano v : volcanoes) {
+
+            Correlation correlation = new Correlation(v);
+            for(Earthquake e : earthquakes) {
+                if(Utils.calculateDistanceBetween(v.getLatitude(), v.getLongitude(), e.getLatitude(), e.getLongitude())<=maxDist) {
+                    correlation.addDependentEarthquake(e);
+                }
+            }
+            if(correlation.getEarthquakes().size()>0) {
+                correlations.add(correlation);
+                System.out.println("Correlation: " + correlation.getVolcano().getLocation() + "-> " + correlation.getEarthquakes().size() + " earthquakes");
+                index ++;
+            }
+        }
+        System.out.println("Approx " + (double) (index * 100 / volcanoes.size()) + "% of the vulcanoes ("+ index +") depend on earthquakes");
+        return correlations;
     }
 
 }
