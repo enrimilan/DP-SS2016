9ba19f49cc55f7912ab5d98c62229255d81229cd
Enri Miho <enrimiho@gmail.com>
2016-06-17 01:23:52 +0200

    use files locally if can't reach host.

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-sudo rm volcanoes.txt earthquakes.txt
-sudo wget http://volcano.si.edu/includes/tdpmap/volcanoes.txt
-sudo wget http://volcano.si.edu/includes/tdpmap/earthquakes.txt
-sudo java -jar Experiment.jar volcanoes.txt earthquakes.txt 40000
+if curl --output /dev/null --silent --head --fail "http://volcano.si.edu/includes/tdpmap/volcanoes.txt"; then
+  wget -O volcanoes.txt http://volcano.si.edu/includes/tdpmap/volcanoes.txt
+else
+  echo "Can't connect to host to download volcanoes. Looking for local files instead..."
+fi
+
+if curl --output /dev/null --silent --head --fail "http://volcano.si.edu/includes/tdpmap/earthquakes.txt"; then
+  wget -O earthquakes.txt http://volcano.si.edu/includes/tdpmap/earthquakes.txt
+else
+  echo "Can't connect to host to download earthquakes. Looking for local files instead..."
+fi
+
+if [ -f volcanoes.txt ] && [ -f earthquakes.txt ]; then
+  java -jar Experiment.jar volcanoes.txt earthquakes.txt 40000
+else
+  echo "File(s) missing to perform this experiment"
+fi
+
+
+
