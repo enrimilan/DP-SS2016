eff28735f5c5b207c001f1f4a165d42a092ec2c4
Enri Miho <enrimiho@gmail.com>
2016-06-16 18:58:50 +0200

    40km

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -3,4 +3,4 @@
 sudo rm volcanoes.txt earthquakes.txt
 sudo wget http://volcano.si.edu/includes/tdpmap/volcanoes.txt
 sudo wget http://volcano.si.edu/includes/tdpmap/earthquakes.txt
-sudo java -jar Experiment.jar volcanoes.txt earthquakes.txt 15000
+sudo java -jar Experiment.jar volcanoes.txt earthquakes.txt 40000
